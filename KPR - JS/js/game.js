c4a66c4c801be28691c130dcf72c31c6116cc742
Dušan Riečičiar <81569486+dusickar@users.noreply.github.com>
2022-11-08 19:46:39 +0100

    Add files via upload

--- KPR - JS/js/game.js
+++ KPR - JS/js/game.js
@@ -1,12 +1,14 @@
-var btn = document.getElementById(“play-game”);
-
-function(game) {
-    var kameň = kameň;
-    var papier = papier;
-    var nožnice = nožnice;
-
-
+function game() {
+    var playGame = document.getElementById(playGame);
+    alert('Hra začína! Zadaj hodnotu (kamen, papier, noznice) a uvidíš či si vyhral!');
+    let choice = parseInt(prompt('Tvoja voľba?'));
+    kpr = ['kamen',  'papier',  'noznice'];
+    console.log(choice);
+    
 };
+
+playGame.addEventListener('click', game);
+
 
 
 
