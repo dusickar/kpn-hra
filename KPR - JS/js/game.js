f9150d54bfc72564f6abc8ccf4c19be484018fd7
Dušan Riečičiar <81569486+dusickar@users.noreply.github.com>
2022-11-18 23:07:31 +0100

    Add files via upload

--- KPR - JS/js/game.js
+++ KPR - JS/js/game.js
@@ -1,160 +1,82 @@
 document.getElementById("playGame");
 
-function game( result ) {
+function game() {
  
     alert('Hra začína! Zadaj hodnotu (k, p, n) a uvidíš či si vyhral!');
 
-    const oneCall = function PcChoice () {
-        console.log(oneCall);
-    };
-
-        //Voľba PC
-        const PcChoice = (length = 1) => {
-            // Declare all characters
-            let chars = 'kpn';
-        
-            // Pick characters randomly
-            let str = '';
-            for (let i = 0; i < length; i++) {
-                str += chars.charAt(Math.floor(Math.random() * chars.length));
-            }
-            console.log('PC : ' + str ) 
-                           
-            return str;
-        };
-    // };
-
-
     
     //Voľba usera
-    let userChoice = prompt('Tvoja voľba?');
-    kpr = ['k',  'p',  'n'];
+    const oneCall = PcChoice();
+    const userChoice = prompt('Tvoja voľba?');
+    kpn = ['k',  'p',  'n'];
     console.log('User : ', userChoice);
-    ;
 
-    alert('Voľba PC : ' + PcChoice());
+    alert('Voľba PC : ' + oneCall);
+
+
+    //Voľba PC
+    function PcChoice() {
+        const length = 1; 
+        // Declare all characters
+        const chars = 'kpn';
+    
+        // Pick characters randomly
+        let str = '';
+        for (let i = 0; i < length; i++) {
+            str += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+        console.log('PC : ' + str ) 
+                        
+        return str;
+    } 
 
 
     //Výsledok hry
-    function result() {
-        console.log(PcChoice);
-        console.log(userChoice);
-        console.log(result);
-        alert(result);
 
-
-        // KAMEŇ
-        if (userChoice == 'k' && PcChoice == 'k') {
-            console.log('Remíza!')
-            alert('Remíza!');
-            alert('Pre ďalšie kolo klikni na OK.');
-        } else if (userChoice == 'k' && PcChoice == 'n') {
-            console.log('Výhra user!')
-            alert('Výhra');
-        } else if (userChoice == 'k' && PcChoice == 'p') {
-            console.log('Prehra user!')
-            alert('Prehra!'); 
-        };
-
-        //PAPIER
-        if (userChoice == 'p' && PcChoice == 'p') {
-            console.log('Remíza!')
-            alert('Remíza!');
-            alert('Pre ďalšie kolo klikni na OK.');
-        } else if (userChoice == 'p' && PcChoice == 'k') {
-            console.log('Výhra user!')
-            alert('Výhra');
-        } else if (userChoice == 'p' && PcChoice == 'n') {
-            console.log('Prehra user!')
-            alert('Prehra!'); 
-        };
-
-        //NOŽNICE
-        if (userChoice == 'n' && PcChoice == 'n') {
-            console.log('Remíza!')
-            alert('Remíza!');
-            alert('Pre ďalšie kolo klikni na OK.');
-        } else if (userChoice == 'n' && PcChoice == 'p') {
-            console.log('Výhra user!')
-            alert('Výhra');
-        } else if (userChoice == 'n' && PcChoice == 'k') {
-            console.log('Prehra user!')
-            alert('Prehra!'); 
-        };
-        return result;
+    // KAMEŇ
+    if (userChoice === 'k' && oneCall === 'k') {
+        console.log('Remíza!')
+        alert('Remíza!');
+        
+    } else if (userChoice === 'k' && oneCall === 'n') {
+        console.log('Výhra user!')
+        alert('Výhra');
+    } else if (userChoice === 'k' && oneCall === 'p') {
+        console.log('Prehra user!')
+        alert('Prehra!'); 
     };
+
+    //PAPIER
+    if (userChoice === 'p' && oneCall === 'p') {
+        console.log('Remíza!')
+        alert('Remíza!');
+        alert('Pre ďalšie kolo klikni na OK.');
+    } else if (userChoice === 'p' && oneCall === 'k') {
+        console.log('Výhra user!')
+        alert('Výhra');
+    } else if (userChoice === 'p' && oneCall === 'n') {
+        console.log('Prehra user!')
+        alert('Prehra!'); 
+    };
+
+    //NOŽNICE
+    if (userChoice === 'n' && oneCall === 'n') {
+        console.log('Remíza!')
+        alert('Remíza!');
+        alert('Pre ďalšie kolo klikni na OK.');
+    } else if (userChoice === 'n' && oneCall === 'p') {
+        console.log('Výhra user!')
+        alert('Výhra');
+    } else if (userChoice === 'n' && oneCall === 'k') {
+        console.log('Prehra user!')
+        alert('Prehra!'); 
+    };
+
+    alert('Pre ďalšie kolo klikni na OK.');
 };
 
-// };
-
-    // // const resultP = 'p';
-    // switch (resultP) {
-    //     case 'k':
-    //         console.log('Výhra user!')
-    //         alert('Výhra')
-    //         alert('Pre ďalšie kolo klikni na OK.');
-    //         break;
-    //     case 'p' :
-    //         console.log('Remíza!')
-    //         alert('Remíza!')
-    //         alert('Pre ďalšie kolo klikni na OK.');
-    //         break;
-    //     case 'n' : 
-    //         console.log('Prehra user!')
-    //         alert('Prehra!')
-    //         alert('Pre ďalšie kolo klikni na OK.');
-    //         break;
-    // }
-    // // const resultN = 'n';
-    // switch (resultN) {
-    //     case 'k':
-    //         console.log('Prehra user!')
-    //         alert('Prehra!')
-    //         alert('Pre ďalšie kolo klikni na OK.');
-    //         break;
-    //     case 'p' :
-    //         console.log('Výhra user!')
-    //         alert('Výhra!')
-    //         alert('Pre ďalšie kolo klikni na OK.');
-    //         break;
-    //     case 'n' : 
-    //         console.log('Remíza!')
-    //         alert('Remíza!')
-    //         alert('Pre ďalšie kolo klikni na OK.');
-    //         break;
-    // }
-// };
-
-
-
-
-
-
-
-//     if (userChoice === 'p')
-//         if (PcChoice === 'k')
-//             alert('Vyhral si!')
-//             console.log('Výhra user!')
-//                 alert('Pre ďalšie kolo klikni na OK.');
-
-//     if (userChoice === 'n')
-//         if (PcChoice === 'p')
-//             alert('Vyhral si!')
-//             console.log('Výhra user!')
-//                 alert('Pre ďalšie kolo klikni na OK.');
-// };
-
-
-
-
-
-
-    // alert('Prehral si!');
-    // alert('Remíza');
-    
 
 playGame.addEventListener('click', game);
-
 
 
 
