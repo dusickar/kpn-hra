1b8d3d760883cc6335f2274fc6b314e948dc73e4
Dušan Riečičiar <81569486+dusickar@users.noreply.github.com>
2022-11-21 19:52:39 +0100

    Add files via upload

--- KPN - JS/js/game.js
+++ KPN - JS/js/game.js
@@ -4,38 +4,38 @@ document.getElementById("nextGame");
 function game() {
  
     alert('Hra začína! Zadaj hodnotu (k, p, n) a uvidíš či si vyhral!');
-
     
-   
+    
+    
     //Voľba usera
     const oneCall = PcChoice();
     const userChoice = prompt('Tvoja voľba?');
     kpn = ['k',  'p',  'n'];
     console.log('User : ', userChoice);
-
+    
     alert('Voľba PC : ' + oneCall);
-
-
+    
+    
     //Voľba PC
     function PcChoice() {
         const length = 1; 
         // Declare all characters
         const chars = 'kpn';
-    
+        
         // Pick characters randomly
         let str = '';
         for (let i = 0; i < length; i++) {
             str += chars.charAt(Math.floor(Math.random() * chars.length));
         }
         console.log('PC : ' + str ) 
-                        
+        
         return str;
     } 
-
-   
+    
+    
     
     //Výsledok hry
-    let userScore = 0;
+    scoreUser = 0;
     // scoreUser = scoreUser.textContent;
     // scoreUser = Number(scoreUser + 1);
     // scoreUser = scoreUser.textContent;
@@ -43,19 +43,19 @@ function game() {
     if (userChoice === 'k' && oneCall === 'n') {
         console.log('Výhra user!')
         alert('Výhra');
-        userScore++;
+        scoreUser++;
     } else if (userChoice === 'p' && oneCall === 'k') {
         console.log('Výhra user!')
         alert('Výhra');
-        userScore++;
+        scoreUser++;
     } else if (userChoice === 'n' && oneCall === 'p') {
         console.log('Výhra user!')
         alert('Výhra');
-        userScore++;
+        scoreUser++;
     };
     
     
-
+    
     // KAMEŇ
     if (userChoice === 'k' && oneCall === 'k') {
         console.log('Remíza!')
@@ -72,21 +72,21 @@ function game() {
     if (userChoice === 'p' && oneCall === 'p') {
         console.log('Remíza!')
         alert('Remíza!');
-    // } else if (userChoice === 'p' && oneCall === 'k') {
-    //     console.log('Výhra user!')
-    //     alert('Výhra');
+        // } else if (userChoice === 'p' && oneCall === 'k') {
+            //     console.log('Výhra user!')
+            //     alert('Výhra');
     } else if (userChoice === 'p' && oneCall === 'n') {
         console.log('Prehra user!')
         alert('Prehra!'); 
     };
-
+    
     //NOŽNICE
     if (userChoice === 'n' && oneCall === 'n') {
         console.log('Remíza!')
         alert('Remíza!');
     // } else if (userChoice === 'n' && oneCall === 'p') {
-    //     console.log('Výhra user!')
-    //     alert('Výhra');
+        //     console.log('Výhra user!')
+        //     alert('Výhra');
     } else if (userChoice === 'n' && oneCall === 'k') {
         console.log('Prehra user!')
         alert('Prehra!'); 
@@ -98,22 +98,22 @@ function game() {
     
     // scoreUser.addEventListener(win(), function() {
     
-    //    const userscoreElement = this.nextElementSibling,
-    //        userscore = Number( userscoreElement.textContent );
-    
-    //        userscore = userscore + 1;
-    //        userscoreElement.textContent = userscore;
-    
-    
+        //    const userscoreElement = this.nextElementSibling,
+        //        userscore = Number( userscoreElement.textContent );
+        
+        //        userscore = userscore + 1;
+        //        userscoreElement.textContent = userscore;
+        
+        
     //    console.log( this );
     
     // });
-
-
-
+    
+    
+    
 }; 
 
-document.getElementById("userscore").innerHTML = userScore;
+scoreUser = document.getElementById("userscore").innerHTML
 // document.getElementById("pcscore").innerHTML = scorePC
 
 playGame.addEventListener('click', game);
